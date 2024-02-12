3124047fc205b43e9833d11e81f681b497693531
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-12 15:50:17 -0500

    Added endpoints

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {"message": "Root Endpoint"}
+
+@app.get("/{id}")
+async def get_note(id: int):
+    print(id)
+    return {"message": "Get Note Endpoint"}
+
+@app.post("/")
+async def create_note():
+    return {"message": "Create Note Endpoint"}
+
+@app.put("/{id}")
+async def update_note(id: int):
+    print(id)
+    return {"message": "Update Note Endpoint"}
+
+@app.delete("/{id}")
+async def delete_note(id: int):
+    print(id)
+    return {"message": "Delete Note Endpoint"}
